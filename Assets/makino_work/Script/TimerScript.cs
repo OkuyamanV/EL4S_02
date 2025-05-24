fd9066c5a2bbf99ce8f4fb3a00c9c640ed6c22ed
Makino_Laptop <hakkilost@icloud.com>
2025-05-24 10:50:28 +0900

    タイマー完成

--- Assets/makino_work/Script/TimerScript.cs
+++ Assets/makino_work/Script/TimerScript.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 //全コピペ！！！！！
 
@@ -10,41 +11,53 @@ public class TimerScript : MonoBehaviour
 {
 
     [SerializeField, Header("制限時間の設定値")]
-    public int battleTime;
+    public int playTime;
     [SerializeField, Header("制限時間の表示")]
-    public TMP_Text battleTimeText;
+    public TMP_Text playTimeText;
 
     private int currentTime;              // 現在の残り時間（不要な場合は宣言しない）
     private float timer;                  // 時間計測用
 
     void Start()
     {
-        // currentTimeを利用する場合にはここで代入する。以下、必要に応じてbattleTimeをcurrentTimeに書き換える
-        currentTime = battleTime;
-        DisplayBattleTime(battleTime);
+        // currentTimeを利用する場合にはここで代入する。以下、必要に応じてplayTimeをcurrentTimeに書き換える
+        currentTime = playTime;
+        DisplayPlayTime(playTime);
     }
 
     void Update()
     {
         // timerを利用して経過時間を計測
         timer += Time.deltaTime;
-        // 1秒経過ごとにtimerを0に戻し、battleTime(currentTime)を減算する
+        // 1秒経過ごとにtimerを0に戻し、playTime(currentTime)を減算する
         if (timer >= 1)
         {
             timer = 0;
-            battleTime--;  // あるいは、currentTime--;
+            playTime--;  // あるいは、currentTime--;
             // 時間表示を更新するメソッドを呼び出す
-            DisplayBattleTime(battleTime);   // あるいは、DisplayBattleTime(currentTime);
+            DisplayPlayTime(playTime);   // あるいは、DisplayPlayTime(currentTime);
+        }
+
+        if (playTime <= 0)
+        {
+            //ゲームオーバー処理
+            SceneManager.LoadScene("testGameover");
+        }
+
+        if (playTime < 0)
+        {
+            //一応マイナス値いかないように
+            playTime = 0;
         }
     }
 
     /// <summary>
     /// 制限時間を更新して[分:秒]で表示する
     /// </summary>
-    private void DisplayBattleTime(int limitTime)
+    private void DisplayPlayTime(int limitTime)
     {
         // 引数で受け取った値を[分:秒]に変換して表示する
         // ToString("00")でゼロプレースフォルダーして、１桁のときは頭に0をつける
-        battleTimeText.text = ((int)(limitTime / 60)).ToString("00") + ":" + ((int)limitTime % 60).ToString("00");
+        playTimeText.text = ((int)(limitTime / 60)).ToString("00") + ":" + ((int)limitTime % 60).ToString("00");
     }
 }
